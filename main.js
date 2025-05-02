7a9647b5676c4910b3a76efe344c85870c526f20
Your Name <you@example.com>
2025-05-02 11:34:47 +0500

    feat: add responsive slider section

---
+++ main.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+    $('.slider-wrapper').each(function() {
+        const wrapper = $(this);
+        const slider = wrapper.find('.splide-slider');
+        const prevBtn = wrapper.find('.slider-prev')[0];
+        const nextBtn = wrapper.find('.slider-next')[0]; 
+
+        const options = {
+            type: 'loop',
+            // gap: '1rem',
+            autoplay: false,
+        };
+
+        let extraOptions = {};
+
+        if (slider.hasClass('production')) {
+            extraOptions = {
+                focus: 0,
+                interval: 4000,
+                pagination: false,
+                arrows: false,
+                perPage: 3,
+                breakpoints: {
+                    1200: {
+                        perPage: 2,
+                    },
+                    768: {
+                        perPage: 1.2,
+                    },
+                    480: {
+                        perPage: 1,
+                    },
+                },
+            };
+        } else if (slider.hasClass('tours')) {
+            extraOptions = {
+                focus: 0,
+                interval: 4000,
+                pagination: false,
+                arrows: false,
+                perPage: 3.8, 
+                breakpoints: {
+                    1600: {
+                        perPage: 3,
+                    },
+                    990: {
+                        perPage: 2.3,
+                    },
+                    582: {
+                        perPage: 1.5,
+                    },
+                    480: {
+                        perPage: 1,
+                    },
+                },
+            };
+        }else if (slider.hasClass('reviews')) {
+            extraOptions = {
+                focus: 0,
+                interval: 4000,
+                pagination: false,
+                arrows: false,
+                perPage: 3, 
+                breakpoints: {
+                    1600: {
+                        perPage: 2.5,
+                    },
+                    990: {
+                        perPage: 2,
+                    },
+                    768: {
+                        perPage: 1.5,
+                    },
+                    480: {
+                        perPage: 1,
+                    },
+                },
+            };
+        }
+        const finalOptions = {...options, ...extraOptions };
+        const splide = new Splide(slider[0], finalOptions);
+
+        if (prevBtn) prevBtn.addEventListener('click', () => splide.go('-1'));
+        if (nextBtn) nextBtn.addEventListener('click', () => splide.go('+1'));
+
+        splide.mount();
+    });
+});
